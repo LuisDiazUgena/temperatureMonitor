a369b3deafa145af99aedc2ea60f980d6b0cf97f
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2018-07-01 03:43:07 +0200

    tempInstall & tempStart updates
    Update tempinstall.sh with a different code to insert another one line into rc.local to start the script directly through python.
    Deletion tempStart.sh because it will not use anymore.

--- tempInstall.sh
+++ tempInstall.sh
@@ -8,5 +8,4 @@ sudo apt-get install python-pkg-resources python3-pkg-resources -y
 sudo apt-get install build-essential python-dev python-smbus python-pip -y
 cd ~
 sudo chmod 755 /home/pi/temperatureMonitor/Pngview/pngview
-sudo chmod 755 /home/pi/temperatureMonitor/tempStart.sh
-sudo sed -i '/\"exit 0\"/!s/exit 0/\/home\/pi\/temperatureMonitor\/tempStart.sh \&\nexit 0/g' /etc/rc.local
+sudo sed -i '/\"exit 0\"/!s/exit 0/\(sudo python \/home\/pi\/temperatureMonitor\/tempMonitor.py) \&\nexit 0/g' /etc/rc.local
